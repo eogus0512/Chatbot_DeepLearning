a484e7b6310cdd7ac6c36397dba062cc65b1f60e
Daehyun <eogus0512@gmail.com>
2021-01-17 11:05:39 +0900

    create ner model

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import platform
+from konlpy.tag import Komoran
+import numpy as np
 
-print(platform.architecture())
+komoran = Komoran()
+
+query = "가락지빵 할게요"
+pos = komoran.pos(query)
+print("; " + query + "\n")
+for a in pos:
+    i = 1
+    print(str(i) + "\t" + a[0] + "\t" + a[1])
+    int(i)
+print(pos)
+pred_index = np.argmax(query)
+print(pred_index)
